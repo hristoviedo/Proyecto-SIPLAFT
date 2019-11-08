4998af1ad2c0e092fee8126c3b2aa006a6ddccc5
Hristo Stochkov Oviedo Santos <hristo.oviedohn@gmail.com>
2019-11-08 16:58:03 -0600

    Update home_col.blade.php
    Se cambiaron los labels para una mejor comprensión

--- resources/views/home_col.blade.php
+++ resources/views/home_col.blade.php
@@ -3,7 +3,7 @@
 @section('content.col') {{-- Inicio de la seccion --}}
   <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4"> {{-- Inicio del main --}}
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
-        <h1 class="h2">Subir archivo</h1>
+        <h1 class="h2">Enviar archivo de clientes</h1>
     </div>
     <div class="mostrar_ocultar">
         {{-- Inicio de la condición --}}
@@ -20,17 +20,21 @@
 
             @csrf {{-- Token generado --}}
 
-            {{-- Input de tipo File que solo acepta archivos de excel y es requerido --}}
+            <div>
+              {{-- Input de tipo File que solo acepta archivos de excel y es requerido --}}
             <input type="file" accept=".xlsx" name="file" class="custom-file-input" id="customFile" required>
 
             {{-- Label que cambia según el nombre del archivo (pendiente de configurar js) --}}
             <label class="custom-file-label" for="customFile"></label>
+            </div>
 
-            {{-- Botón para subir la lista de clientes --}}
-            <button class="btn btn-block btn-subir btn-color" id="button">
+            <div class="text-center">
+              {{-- Botón para subir la lista de clientes --}}
+            <button class="btn btn-lg  btn-subir btn-color" id="button">
               {{-- <span class="spinner-border spinner-border-sm mr-2"></span> --}}
-              Importar Clientes
+              Enviar
             </button>
+            </div>
         </form>{{-- Fin del formulario --}}
     </div>
   </main> {{-- Fin del main --}}
