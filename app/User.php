7663dc815027915594ff4142c929e156368f3396
Hristo Stochkov Oviedo Santos <hristo.oviedohn@gmail.com>
2019-11-11 16:52:04 -0600

    Tabla intermedia
    Se agregó una tabla intermedia entre clientes y usuarios y se modificó a nivel de laravel.

--- app/User.php
+++ app/User.php
@@ -44,4 +44,8 @@ class User extends Authenticatable
     public function role(){
         return $this->belongsTo(Role::class);
     }
+    
+    public function clientes(){
+        return $this->belongsToMany(Cliente::class)->withTimestamps();
+    }
 }
