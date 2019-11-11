1180303f607bc2997690eef37fe85ab21975c97c
Hristo Stochkov Oviedo Santos <hristo.oviedohn@gmail.com>
2019-11-11 15:12:58 -0600

    Migraciones
    Se crearon las tablas de role y company

--- app/User.php
+++ app/User.php
@@ -36,4 +36,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function company(){
+        return $this->belongsTo(Company::class);
+    }
+
+    public function role(){
+        return $this->belongsTo(Role::class);
+    }
 }
--- database/migrations/2019_11_11_181811_add_role_id_at_users.php
+++ database/migrations/2019_11_11_181811_add_role_id_at_users.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRolesTable extends Migration
+class AddRoleIdAtUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,13 @@ class CreateRolesTable extends Migration
      */
     public function up()
     {
-        Schema::create('roles', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('role_name');
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table){
+
+            $table->bigInteger('role_id')->unsigned()->nullable()->after('id');
+
+            $table->foreign('role_id')->references('id')->on('roles')
+                ->onDelete('set null')
+                ->onUpdate('cascade');
         });
     }
 
@@ -27,6 +30,6 @@ class CreateRolesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('roles');
+        //
     }
 }
