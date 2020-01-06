365eee2064789ab0e9ae8e7d41e2e75a7078b327
Hristo Stochkov Oviedo Santos <hristo.oviedohn@gmail.com>
2020-01-06 12:18:50 -0600

    Create activeMiddleware.php
    Creación del middleware para dar acceso a usuarios activos

---
+++ app/Http/Middleware/activeMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class activeMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        //Inicio de la condición
+        if(Auth::user()->active =='1'){             //se valida si está activo
+            return $next($request);                     //Deja pasar la petición
+        }else{
+            Auth::logout();
+            return redirect('/');                   //Redirecciona a la pagina de inicio
+        }//Fin de la condicion
+    }// Fin de la función
+}// Fin de la clase
