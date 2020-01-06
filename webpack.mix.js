bde9ba9cf5b2f06674c8a3f8e5ff6fd8f1cc8083
Hristo Stochkov Oviedo Santos <hristo.oviedohn@gmail.com>
2020-01-06 12:21:36 -0600

    Delete
    Se eliminaron archivos de tipo .css y .js, luego se acctualizó el webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,15 +4,13 @@ const mix = require('laravel-mix');
 mix.js([
         'resources/js/jquery.js',
         'resources/js/bootstrap.js',
-        'resources/js/alertify.js',
         'resources/js/app.js'
     ],
         'public/js/app.js')
     // .sass('resources/sass/app.scss',
     //     'public/css/app.css')
     .styles([
-            'resources/css/style.css',
-            'resources/css/alertify.css'],
+            'resources/css/style.css'],
             'public/css/styles.css');
 
 mix.disableNotifications();
