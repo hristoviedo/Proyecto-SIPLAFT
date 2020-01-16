005b434dc6bdadc81e87874043aeeea10022ff8f
Hristo Stochkov Oviedo Santos <hristo.oviedohn@gmail.com>
2020-01-16 09:15:49 -0600

    Update ClientsUpload.php
    Se agregaron dos campos a la lista de de subida masiva

--- app/ClientsUpload.php
+++ app/ClientsUpload.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class ClientsUpload extends Model
 {
     //Permite la subida masiva de información a la base de datos. Deben estar todos los campos de la tabla clients
-    protected $fillable = ['identity','name','age','email','workplace','phone1','phone2','nationality','households','total_amount','activity','funding'];
+    protected $fillable = ['identity','name','age','email','workplace', 'workstation', 'salary','phone1','phone2','nationality','households','total_amount','activity','funding'];
 }
