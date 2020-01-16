2e0c0861276d46961915d3163c92270bcaf42c61
Hristo Stochkov Oviedo Santos <hristo.oviedohn@gmail.com>
2020-01-16 09:37:21 -0600

    Update ClientImport.php
    Se modificó el configuración de importación para clients_uploads

--- app/Imports/ClientImport.php
+++ app/Imports/ClientImport.php
@@ -20,7 +20,7 @@ class ClientImport implements ToModel, WithBatchInserts, WithChunkReading
     public function model(array $row)
     {
         //Inicio de la condición
-        if (((!isset($row[0]))) or (($row[2] == 0) and ($row[8] == 0))) { //Verifica si el registro tiene vacío el campo identity o si los campos de tipo enteros son 0
+        if (((!isset($row[0]))) or (($row[2] == 0) and ($row[10] == 0))) { //Verifica si el registro tiene vacío el campo identity o si los campos de tipo enteros son 0
             return null; //No guarda el registro y salta el siguiente
         }//Fin de la condición
 
@@ -30,18 +30,21 @@ class ClientImport implements ToModel, WithBatchInserts, WithChunkReading
         $age = $row[2];
         $email = $row[3];
         $workplace = $row[4];
-        $phone1 = $row[5];
-        $phone2 = $row[6];
-        $nationality = $row[7];
-        $households = $row[8];
-        $total_amount = $row[9];
-        $activity = $row[10];
-        $funding = $row[11];
+        $workstation = $row[5];
+        $salary = $row[6];
+        $phone1 = $row[7];
+        $phone2 = $row[8];
+        $nationality = $row[9];
+        $households = $row[10];
+        $total_amount = $row[11];
+        $activity = $row[12];
+        $funding = $row[13];
 
         //Cambia el tipo de variable para que coincida con los predefinidos en la base de datos
-        $age = (int)$age;
-        $households = (int)$households;
-        $total_amount = (float)$total_amount;
+        $age            = (int)$age;
+        $salary         = (float)$salary;
+        $households     = (int)$households;
+        $total_amount   = (float)$total_amount;
 
         //Realiza la inserción del registro con datos en mayúsculas y sin espacios al principio o final
         return new ClientsUpload([
@@ -50,6 +53,8 @@ class ClientImport implements ToModel, WithBatchInserts, WithChunkReading
             'age'           => $age,
             'email'         => trim($email),
             'workplace'     => trim(mb_strtoupper($workplace,'UTF-8')),
+            'workstation'   => trim(mb_strtoupper($workstation,'UTF-8')),
+            'salary'        => $salary,
             'phone1'        => trim(strtoupper($phone1)),
             'phone2'        => trim(strtoupper($phone2)),
             'nationality'   => trim(mb_strtoupper($nationality,'UTF-8')),
