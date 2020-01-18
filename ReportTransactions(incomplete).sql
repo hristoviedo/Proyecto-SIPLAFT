d30c00545f131f9a2763a44f81c18126abd33273
Hristo Stochkov Oviedo Santos <hristo.oviedohn@gmail.com>
2020-01-18 12:16:33 -0600

    Update ReportTransactions(incomplete).sql
    Ajustes a la consulta base para los reportes

--- ReportTransactions(incomplete).sql
+++ ReportTransactions(incomplete).sql
@@ -5,16 +5,12 @@ tr.transaction_apartment_number as No_DE_APARTAMENTO,
 tr.transaction_operation_date as FECHAS_DE_OPERACION,
 tr.transaction_amount as MONTO_DE_OPERACION,
 tr.transaction_transfer_date as FECHA_DE_TRASPASO_DE_ESCRITURA,
-tr.transaction_intermediary_bank as BANCO_INTERMEDIARIO,
-if(fu.id = 5,"FINANCIAMIENTO","CONTADO") as FONDOS,
-/*fu.name as FONDOS,*/
-/*fu.name as FORMA_DE_PAGO,*/
+if(tr.transaction_intermediary_bank = "CONTADO","",tr.transaction_intermediary_bank) as BANCO_INTERMEDIARIO,
+if(fu.id = 5,"FINANCIAMIENTO","") as FONDOS,
 if(fu.id != 5, fu.name, "") as FORMA_DE_PAGO,
 tr.workplace as LUGAR_DE_TRABAJO,
 if(cl.activity_id = 2, tr.workstation, "") as PUESTO,
 if(cl.activity_id = 2, tr.salary, "") as SALARIO,
-/*tr.workstation as PUESTO,
-tr.salary as SALARIO,*/
 ac.name as FUENTE_DE_INGRESO,
 cl.age as EDAD,
 cl.identity as IDENTIDAD,
