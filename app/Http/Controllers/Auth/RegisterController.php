d8f5d653af16e85e0ae2934ac0acc030225b1341
Hristo Stochkov Oviedo Santos <hristo.oviedohn@gmail.com>
2020-02-03 15:28:35 -0600

    Update RegisterController.php
    Se corrigió los errores que impedían crear un usuario.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\UserValidation;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Request;
 use Illuminate\Foundation\Auth\RegistersUsers;
@@ -64,28 +65,25 @@ class RegisterController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'role_id' => 'required|integer|confirmed',
-            'company_id' => 'required|integer|confirmed',
-            'active' => 'required|boolean|confirmed',
+            'email' => 'required|email|max:255|unique:users',
+            'role_id' => 'required|integer',
+            'company_id' => 'required|integer',
+            'active' => 'required|boolean',
             'password' => 'required|string|min:8|confirmed',
-            'password_confirmation' => 'required_with:password|same:password|string|min:8|confirmed',
+            'password_confirmation' => 'required_with:password|same:password|string|min:8',
         ]);
         if ($validator->fails()) {
-            return redirect('adm.create.user')
-                        ->withErrors($validator)
-                        ->withInput();
-        }
-        else{
-            User::create([
-                'name' => trim(mb_strtoupper($request['name'])),
-                'email' => $request['email'],
-                'role_id' => $request['role_id'],
-                'company_id' => $request['company_id'],
-                'active' => $request['active'],
-                'password' => Hash::make($request['password']),
-            ]);
-            return back()->with('message', 'Usuario registrado'); //Retorna a la página anterior cuando registra al usuario
-        }
+            return redirect('adm.create.user')->withInput()->withErrors($validator);
+        };
+
+        User::create([
+            'name' => trim(mb_strtoupper($request['name'])),
+            'email' => $request['email'],
+            'role_id' => $request['role_id'],
+            'company_id' => $request['company_id'],
+            'active' => $request['active'],
+            'password' => Hash::make($request['password']),
+        ]);
+        return back()->with('message', 'Usuario registrado'); //Retorna a la página anterior cuando registra al usuario
     }
 }
