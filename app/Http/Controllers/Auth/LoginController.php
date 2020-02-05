8672505fa870dcaeb3f97f069a467a27c60e5aff
Hristo Stochkov Oviedo Santos <hristo.oviedohn@gmail.com>
2020-02-05 16:36:36 -0600

    Update LoginController.php
    Se sobreescribieron los métodos por defecto para agregar el evento EventsSIPLAFT

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Carbon\Carbon;
+use App\Events\EventsSIPLAFT;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+
 
 class LoginController extends Controller
 {
@@ -35,5 +41,83 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+            return $this->sendLoginResponse($request);
+        }
+
+        $emailUser=DB::table('users')->where('email','=',$request->email)->value('id');
+        $user_modifier_id = $emailUser;
+        $record_date = Carbon::now()->toDateTimeString();
+        $record_modified_table = null;
+        $record_action = 'FALLÓ INICIO DE SESIÓN';
+        $record_modified_register = null;
+        $record_modified_field = null;
+        $record_new_data = null;
+        $record_old_data = null;
+        $data = array( 'user_modifier_id' => $user_modifier_id, 'record_action' => $record_action, 'record_date' => $record_date , 'record_modified_table' => $record_modified_table, 'record_modified_register' => $record_modified_register, 'record_modified_field'=> $record_modified_field, 'record_new_data' => $record_new_data, 'record_old_data' => $record_old_data );
+        event( new EventsSIPLAFT( $data ));
+        $findLastRecord = DB::table('records')->latest('id')->first();
+        $deleteLastRecord = DB::table('records')->delete($findLastRecord->id);
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
+    protected function authenticated(Request $request, $user)
+    {
+        $user_modifier_id = Auth::user()->id;
+        $record_date = Carbon::now()->toDateTimeString();
+        $record_modified_table = null;
+        $record_action = 'INICIÓ SESIÓN';
+        $record_modified_register = null;
+        $record_modified_field = null;
+        $record_new_data = null;
+        $record_old_data = null;
+        $data = array( 'user_modifier_id' => $user_modifier_id, 'record_action' => $record_action, 'record_date' => $record_date , 'record_modified_table' => $record_modified_table, 'record_modified_register' => $record_modified_register, 'record_modified_field'=> $record_modified_field, 'record_new_data' => $record_new_data, 'record_old_data' => $record_old_data );
+        event( new EventsSIPLAFT( $data ));
+        $findLastRecord = DB::table('records')->latest('id')->first();
+        $deleteLastRecord = DB::table('records')->delete($findLastRecord->id);
+    }
+
+    public function logout(Request $request)
+    {
+        $user_modifier_id = Auth::user()->id;
+        $record_date = Carbon::now()->toDateTimeString();
+        $record_modified_table = null;
+        $record_action = 'CERRÓ SESIÓN';
+        $record_modified_register = null;
+        $record_modified_field = null;
+        $record_new_data = null;
+        $record_old_data = null;
+        $data = array( 'user_modifier_id' => $user_modifier_id, 'record_action' => $record_action, 'record_date' => $record_date , 'record_modified_table' => $record_modified_table, 'record_modified_register' => $record_modified_register, 'record_modified_field'=> $record_modified_field, 'record_new_data' => $record_new_data, 'record_old_data' => $record_old_data );
+        event( new EventsSIPLAFT( $data ));
+        $findLastRecord = DB::table('records')->latest('id')->first();
+        $deleteLastRecord = DB::table('records')->delete($findLastRecord->id);
+
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        return $this->loggedOut($request) ?: redirect('/');
     }
 }
