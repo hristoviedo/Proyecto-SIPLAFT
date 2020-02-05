be27dd036e0f54ba2b50a1cb4877909c535f77bb
Hristo Stochkov Oviedo Santos <hristo.oviedohn@gmail.com>
2020-02-05 16:04:46 -0600

    Update 2020_11_15_184307_create_records_table.php
    Se estructuró la tabla de records con los campos necesarios.

--- database/migrations/2020_11_15_184307_create_records_table.php
+++ database/migrations/2020_11_15_184307_create_records_table.php
@@ -14,8 +14,19 @@ class CreateRecordsTable extends Migration
     public function up()
     {
         Schema::create('records', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
+            $table->bigIncrements('id'); //Número de registro en la base de datos (autoincremental)
+            $table->unsignedBigInteger('user_modifier_id')->default(null)->nullable(); //Id del rol (valor por defecto = null, sin signo y nullable)
+            $table->string('record_action',45); //Nombre del usuario (45 caracteres max, not null)
+            $table->string('record_date'); //Fecha en que se realizó la acción
+            $table->string('record_modified_table')->default(null)->nullable();//Tabla que sufrió cambios
+            $table->string('record_modified_register')->default(null)->nullable(); //Registro que sufrió cambios
+            $table->string('record_modified_field')->default(null)->nullable(); //Campo que sufrió cambios
+            $table->string('record_new_data',45)->default(null)->nullable(); //Nombre del usuario (45 caracteres max, not null)
+            $table->string('record_old_data',45)->default(null)->nullable(); //Nombre del usuario (45 caracteres max, not null)
+
+            $table->foreign('user_modifier_id')->references('id')->on('users') //Relación a la tabla roles
+                    ->onDelete('set null') //No borrar usuario
+                    ->onUpdate('cascade'); //Actualizar en cascada
         });
     }
 
