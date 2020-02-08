296ebd8831b374ef9340872e54987e957d3d1117
Hristo Stochkov Oviedo Santos <hristo.oviedohn@gmail.com>
2020-02-08 09:46:02 -0600

    Update TransactionImport.php
    Se modificó la forma de guardar los datos en la tabla TransactionsUpload

--- app/Imports/TransactionImport.php
+++ app/Imports/TransactionImport.php
@@ -23,29 +23,16 @@ class TransactionImport implements ToModel, WithBatchInserts, WithChunkReading
             return null; //No guarda el registro y salta el siguiente
         }//Fin de la condición
 
-        //Se asigna el valor de cada campo en su respectiva variable
-        $client_identity = $row[0];
-        $transaction_apartment_number = $row[1];
-        $transaction_intermediary_bank = $row[2];
-        $transaction_operation_date = $row[3];
-        $transaction_transfer_date = $row[4];
-        $transaction_cash = $row[5];
-        $transaction_currency = $row[6];
-        $transaction_amount = $row[7];
-
-        //Cambia el tipo de variable para que coincida con los predefinidos en la base de datos
-        $transaction_amount = (float)$transaction_amount;
-
         //Realiza la inserción del registro con datos sin espacios al principio o final
         return new TransactionsUpload([
-            'client_identity'               => trim($client_identity),
-            'transaction_apartment_number'  => trim(mb_strtoupper($transaction_apartment_number)),
-            'transaction_intermediary_bank' => trim(mb_strtoupper($transaction_intermediary_bank)),
-            'transaction_operation_date'    => trim($transaction_operation_date),
-            'transaction_transfer_date'     => trim($transaction_transfer_date),
-            'transaction_cash'              => trim(mb_strtoupper($transaction_cash)),
-            'transaction_currency'          => trim(mb_strtoupper($transaction_currency)),
-            'transaction_amount'            => $transaction_amount,
+            'client_identity'               => trim($row[0]),
+            'transaction_apartment_number'  => trim(mb_strtoupper($row[1])),
+            'transaction_intermediary_bank' => trim(mb_strtoupper($row[2])),
+            'transaction_operation_date'    => trim($row[3]),
+            'transaction_transfer_date'     => trim($row[4]),
+            'transaction_cash'              => trim(mb_strtoupper($row[5])),
+            'transaction_currency'          => trim(mb_strtoupper($row[6])),
+            'transaction_amount'            => (float)$row[7],
         ]);
     }//Fin de la función
 
