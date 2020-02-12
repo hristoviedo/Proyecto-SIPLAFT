61e24239513dff279986d7e60f02f24decaa67f9
Hristo Stochkov Oviedo Santos <hristo.oviedohn@gmail.com>
2020-02-12 10:50:53 -0600

    Update ClientImport.php
    Se cambió toda la lógica para insertar o actualizar clientes sin usar el procedimiento almacenado.

--- app/Imports/ClientImport.php
+++ app/Imports/ClientImport.php
@@ -25,48 +25,61 @@ class ClientImport implements ToModel, WithBatchInserts, WithChunkReading, WithV
         if (((!isset($row[0]))) or $row[0] == 0 or ((!isset($row[13]))) or (((!isset($row[2]))) or ((!isset($row[10]))))) { //Verifica si el registro tiene vacío el campo identity o si los campos de tipo enteros son 0
             return null; //No guarda el registro y salta el siguiente
         }//Fin de la condición
+        else{
+            $client = Client::where('identity','=',trim($row[0]))->first();
+            $activityID = DB::table('activities')->where('name','=',trim(mb_strtoupper($row[12],'UTF-8')))->value('id');
+            $fundingID  = DB::table('fundings')->where('name','=',trim(mb_strtoupper($row[13],'UTF-8')))->value('id');
+        }
+        if(!$client){
+            // dd($client);
+            //Realiza la inserción del registro con datos en mayúsculas y sin espacios al principio o final y con algunos elementos formateados.
+            return new Client([
+                'activity_id'       => $activityID,
+                'funding_id'        => $fundingID,
+                'identity'          => trim($row[0]),
+                'name'              => trim(mb_strtoupper($row[1],'UTF-8')),
+                'age'               => (int)$row[2],
+                'email'             => trim($row[3]),
+                'workplace'         => trim(mb_strtoupper($row[4],'UTF-8')),
+                'workstation'       => trim(mb_strtoupper($row[5],'UTF-8')),
+                'salary'            => (float)$row[6],
+                'phone1'            => trim(strtoupper($row[7])),
+                'phone2'            => trim(strtoupper($row[8])),
+                'nationality'       => trim(mb_strtoupper($row[9],'UTF-8')),
+                'households'        => (int)$row[10],
+                'total_amount'      => (float)$row[11],
+                ]);
+            }else{
+            $previousHouseholds             = $client->households;
+            $previousTotalAamount           = $client->total_amount;
 
-        //Realiza la inserción del registro con datos en mayúsculas y sin espacios al principio o final y con algunos elementos formateados.
-
-
-        return new ClientsUpload([
-            'identity'      => trim($row[0]),
-            'name'          => trim(mb_strtoupper($row[1],'UTF-8')),
-            'age'           => (int)$row[2],
-            'email'         => trim($row[3]),
-            'workplace'     => trim(mb_strtoupper($row[4],'UTF-8')),
-            'workstation'   => trim(mb_strtoupper($row[5],'UTF-8')),
-            'salary'        => (float)$row[6],
-            'phone1'        => trim(strtoupper($row[7])),
-            'phone2'        => trim(strtoupper($row[8])),
-            'nationality'   => trim(mb_strtoupper($row[9],'UTF-8')),
-            'households'    => (int)$row[10],
-            'total_amount'  => (float)$row[11],
-            'activity'      => trim(mb_strtoupper($row[12],'UTF-8')),
-            'funding'       => trim(mb_strtoupper($row[13],'UTF-8')),
-            ]);
-        }//Fin de la función
+            $client->activity_id    = $activityID;
+            $client->funding_id     = $fundingID;
+            $client->name           = trim(mb_strtoupper($row[1],'UTF-8'));
+            $client->age            = (int)$row[2];
+            $client->email          = trim($row[3]);
+            $client->workplace      = trim(mb_strtoupper($row[4],'UTF-8'));
+            $client->workstation    = trim(mb_strtoupper($row[5],'UTF-8'));
+            $client->salary         = (float)$row[6];
+            $client->phone1         = trim(strtoupper($row[7]));
+            $client->phone2         = trim(strtoupper($row[8]));
+            $client->nationality    = trim(mb_strtoupper($row[9],'UTF-8'));
+            $client->households     = $previousHouseholds + (int)$row[10];
+            $client->total_amount   = $previousTotalAamount + (float)$row[11];
+            $client->save();
+        }
+    }//Fin de la función
 
         // Inicio de la función
-        public function batchSize(): int
-        {
-            return 50;
-        }//Fin de la función
+    public function batchSize(): int
+    {
+        return 100;
+    }//Fin de la función
 
-        // Inicio de la función
-        public function chunkSize(): int
-        {
-            return 50;
-        }//Fin de la función
-
-        // public function rules()
-        // {
-        //     return [
-        //         'registration_number' => 'regex:/[A-Z]{3}-[0-9]{3}/',
-        //         'doors' => 'in:2,4,6',
-        //         'years' => 'between:1998,2017'
-        //     ];
-        // }
-
+    // Inicio de la función
+    public function chunkSize(): int
+    {
+        return 100;
+    }//Fin de la función
 
 } //Fin de la clase
