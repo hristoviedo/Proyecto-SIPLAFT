83e49f76a4390a9eb8a44040daa4af262169e349
Hristo Stochkov Oviedo Santos <hristo.oviedohn@gmail.com>
2020-02-13 15:43:18 -0600

    Ruta habilitada
    Las rutas para descargar los manuales están disponibles.

--- resources/views/layouts/adm.blade.php
+++ resources/views/layouts/adm.blade.php
@@ -41,7 +41,7 @@
                         <span><b>Ayuda</b></span>
                     </h6>
                     <li class="nav-item">
-                        <a class="nav-link link__color" href="#">
+                        <a class="nav-link link__color" href="{{ route('adm.manual') }}">
                             <svg  width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle"><circle cx="12" cy="12" r="10"></circle></svg>
                             Manual de Usuario
                         </a>
--- resources/views/layouts/col.blade.php
+++ resources/views/layouts/col.blade.php
@@ -47,7 +47,7 @@
                 </a>
             </li> --}}
             <li class="nav-item"> {{-- Item #3 página de ayuda del colaborador (pendiente) --}}
-              <a class="nav-link link__color" href="#">
+            <a class="nav-link link__color" href="{{ route('col.manual') }}">
                   <svg  width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle"><circle cx="12" cy="12" r="10"></circle></svg>
               Manual de Usuario
               </a>
