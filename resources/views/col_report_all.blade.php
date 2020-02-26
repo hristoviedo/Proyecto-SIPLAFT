fb94c7268c96c677605680aaeca1744770cf72c6
Hristo Stochkov Oviedo Santos <hristo.oviedohn@gmail.com>
2020-02-26 11:31:26 -0600

    Corrección de título de reporte completo

--- resources/views/col_report_all.blade.php
+++ resources/views/col_report_all.blade.php
@@ -5,7 +5,7 @@
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4"> {{-- Inicio del main --}}
     <div class="py-5 text-center">
       {{-- <img src="{{ asset('img/logo.png') }}" class="img-fluid img-small" alt="Responsive image"> --}}
-      <h2>Reporte Total de Transacciones</h2>
+      <h2>Reporte Completo de Transacciones</h2>
       <p class="">En esta sección exportará las transacciones registradas en formato .xlsx</p>
     </div>
     <div class="text-center">
