099a794b19e2d1602d2560da766830e3607b9d32
Hristo Stochkov Oviedo Santos <hristo.oviedohn@gmail.com>
2020-03-03 08:53:59 -0600

    Update col_transaction_form.blade.php
    Vista de formulario para transacciones

--- resources/views/col_transaction_form.blade.php
+++ resources/views/col_transaction_form.blade.php
@@ -2,106 +2,83 @@
 
 @section('content.col') {{-- Inicio de la seccion --}}
 
-<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4"> {{-- Inicio del main --}}
-    <div class="py-5 text-center">
+<div id="col_client" role="main" class="col-md-9 ml-sm-auto col-lg-10 px-5"> {{-- Inicio del main --}}
+    <div class="py-3 text-center">
       {{-- <img :src="imageForm"> --}}
-      <h2>Evaluación Preliminar de Riesgo</h2>
-      <p class=""><strong>Con este formulario calculará el riesgo que representa un posible cliente y tomar las medidas necesarias</strong></p>
+      <h2>Crear Nueva Transacción</h2>
+      <p class=""><strong>Con este formulario creará una nueva transacción</strong></p>
     </div>
-    <div class="row">
-      <div class="col-md-3 order-md-2 mb-4">
-        <h4 class="d-flex justify-content-between align-items-center mb-3">
-          <span class="text-muted">Resultados</span>
-          <span class="badge badge-secondary badge-pill" v-show='scoreRisk'>@{{ formatPrice(scoreRisk) }} de 5.00</span>
-        </h4>
-        <ul class="list-group mb-3">
-          <li class="list-group-item d-flex justify-content-between lh-condensed" v-show='name'>
-            <div>
-              <h6 class="my-0 mx-2">Nombre:</h6>
+    <div class="row justify-content-center">
+      <div class="col-md-10">
+        <h4 class="mb-3">Datos de la transacción</h4>
+        <div class="mostrar_ocultar">
+            {{-- Inicio de la condición --}}
+            @if (count($errors) > 0) {{-- ¿Existe un algún error que mostrar? --}}
+                <div class="alert alert-danger" role="alert">
+                    <button type="button" class="close" data-dismiss="alert">x</button>
+                    <p class="lead">Error al enviar lista de transacciones</p> {{-- Muestra los errores --}}
+                    <ul> {{-- Muestra la lista de errores --}}
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                  </ul>
+                </div>
+            @endif {{-- Fin de la condición --}}
+            {{-- Inicio de la condición --}}
+            @if (Session::has('message')) {{-- ¿Existe un mensaje que mostrar? --}}
+                <div class="alert alert-success" role="alert">
+                    <button type="button" class="close" data-dismiss="alert">x</button>
+                    <p class="lead">{{ Session::get('message') }}</p> {{-- Muestra el mensaje --}}
+                </div>
+            @endif {{-- Fin de la condición --}}
+        </div>
+        @foreach($client as $clientData)
+        <form class="border p-3 form" method="POST" action="/transaction.create/{{ $clientData->id }}">
+            @csrf
+            <div class="form-group row">
+                <div class="col-md-6 mb-3">
+                    <label for="identity">Identidad</label>
+                    <input name="identity" id="identity" value="{{ $clientData->identity }}" class="form-control" type="text" disabled required/>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label for="name">Nombre del Cliente</label>
+                    <input name="name" id="name" value="{{ $clientData->name }}" class="form-control text-uppercase" type="text" disabled required/>
+                </div>
             </div>
-          <span class="text-uppercase"><b>@{{ name }}</b></span>
-          </li>
-          <li class="list-group-item d-flex justify-content-between lh-condensed" v-show='age'>
-            <div>
-              <h6 class="my-0 mx-2">Edad:</h6>
+            <div class="form-group row">
+                <div class="col-md-4 mb-3">
+                    <label for="apartment_number">No de Apartamento</label>
+                    <input name="apartment_number" id="apartment_number" class="form-control text-uppercase" type="text" v-model="apartment_number" maxlength="10" value="{{ old('apartment_number') }}" placeholder="EJEMPLO: 9-9999" autofocus required/>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <label for="operation_date">Fecha de Operación</label>
+                    <input name="operation_date" id="operation_date" class="form-control" type="date" v-model="operation_date" value="{{ old('operation_date') }}" placeholder="dia-mes-año" required/>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <label for="transfer_date">Fecha de Traspaso</label>
+                    <input name="transfer_date" id="transfer_date" class="form-control" type="date" v-model="transfer_date" value="{{ old('transfer_date') }}" placeholder="dia-mes-año" required/>
+                </div>
             </div>
-          <span class="text-uppercase"><b>@{{age}} AÑOS</b></span>
-          </li>
-          <li class="list-group-item d-flex justify-content-between lh-condensed" v-show='households'>
-            <div>
-              <h6 class="my-0 mx-2">No. de Viviendas:</h6>
-            </div>
-            <span class="text-uppercase"><b>@{{households}} VIVIENDAS</b></span>
-          </li>
-          <li class="list-group-item d-flex justify-content-between lh-condensed" v-show='activity'>
-            <div>
-              <h6 class="my-0 mx-2">Actividad Económica:</h6>
-            </div>
-          <span class="text-uppercase"><b>@{{ activity }}</b></span>
-          </li>
-          <li class="list-group-item d-flex justify-content-between lh-condensed" v-show='funding'>
-            <div>
-              <h6 class="my-0 mx-2">Fuente de Financiamiento:</h6>
-            </div>
-          <span class="text-uppercase"><b>@{{ funding }}</b></span>
-          </li>
-          <li class="list-group-item d-flex justify-content-between" :style="riskColor()" v-show='risk'>
-            <div class="">
-              <h6 class="my-0 mx-2"><b>Riesgo</b></h6>
-            </div>
-          <span class="">@{{ risk }}</span>
-          </li>
-        </ul>
-      </div>
-      <div class="col-md-9 order-md-1">
-        <h4 class="mb-3">Datos del posible cliente</h4>
-        <form class="">
-          <div class="row">
-            <div class="col-md-6 mb-3">
-                <label for="name">Nombre completo</label>
-                <input type="text" class="form-control text-uppercase" id="name" placeholder="" v-model="name" maxlength="40" oninput="if(this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" /><i>(Máximo 40 caracteres)</i>
-                {{-- <div class="invalid-feedback">
-                  Valid first name is required.
-                </div> --}}
-              </div>
-            <div class="col-md-3 mb-3">
-              <label for="age">Edad</label>
-              <input type="number" class="form-control" min="0" maxlength="3" id="age" placeholder="" v-model="age" oninput="if(this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" /><i>(Máximo 3 dígitos)</i>
-              {{-- <div class="invalid-feedback">
-                Zip code required.
-              </div> --}}
-            </div>
-            <div class="col-md-3 mb-3">
-                <label class="" for="households">No. de Viviendas</label>
-                <input type="number" class="form-control" id="households" maxlength="2" placeholder=""  v-model="households" oninput="if(this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" /><i>(Máximo 2 dígitos)</i>
-                {{-- <div class="invalid-feedback">
-                  Zip code required.
-                </div> --}}
-              </div>
-          </div>
-          <div class="row">
-              <div class="col-md-6 mb-3">
-                <label for="activity">Actividad Económica</label>
-                <select name="activity" class="custom-select d-block w-100" id="activity"  v-model="activity">
-                    @foreach($activities as $activity)
-                        <option>{{ $activity->name }}</option>
-                    @endforeach
-                </select>
-                {{-- <div class="invalid-feedback">
-                  Please select a valid country.
-                </div> --}}
-              </div>
-              <div class="col-md-6 mb-3">
-                <label for="funding">Fuente de Financiamiento</label>
-                <select name="funding" class="custom-select d-block w-100" v-model="funding">
-                    @foreach($fundings as $funding)
-                        <option>{{ $funding->name }}</option>
-                    @endforeach
-                </select>
-                {{-- <div class="invalid-feedback">
-                  Please provide a valid state.
-                </div> --}}
-              </div>
+            <div class="form-group row">
+                <div class="col-md-3 mb-3">
+                    <label class="" for="intermediary_bank">Banco Intermediario</label>
+                    <input name="intermediary_bank" id="intermediary_bank" class="form-control text-uppercase" v-model="intermediary_bank" maxlength="30" value="{{ old('intermediary_bank') }}" placeholder="EJEMPLO: BAC" onKeyUp="this.value=this.value.toUpperCase();" required/>
+                </div>
+                <div class="col-md-3 mb-3">
+                    <label class="" for="amount">Monto de la Transacción</label>
+                    <input name="amount" id="amount" class="form-control text-uppercase" v-model="amount" v-money="money" maxlength="11" style="text-align: right" value="{{ old('amount') }}" placeholder=""/>
+                </div>
+                <div class="col-md-3 mb-3">
+                    <label class="" for="currency">Moneda</label>
+                    <input name="currency" id="currency" class="form-control text-uppercase" v-model="currency" maxlength="45" value="{{ old('currency') }}" placeholder="EJEMPLO: LEMPIRAS" onKeyUp="this.value=this.value.toUpperCase();"/>
+                </div>
+                <div class="col-md-3 mb-3">
+                    <label class="" for="cash">¿Efectivo?</label>
+                    <select name="cash" class="form-control" v-model="cash">
+                        <option value="1">SÍ</option>
+                        <option value="0">NO</option>
+                    </select>
+                </div>
             </div>
             <div class="row">
                 <div class="col">
@@ -109,14 +86,20 @@
                 <div class="col">
                 </div>
                 <div class="col-auto">
-                  <button type="button" class="btn btn-sm btn-color" @click.prevent='clearProperty'>Limpiar</button>
+                <button type="submit" class="btn btn-success">Registrar</button>
                 </div>
                 <div class="col-auto">
-                  <button type="button" class="btn btn-sm btn-color" @click.prevent='calculateRisk'>Calcular</button>
+                <button type="button" class="btn btn-primary" @click.prevent='clearProperty'>Limpiar</button>
+                </div>
+                <div class="col-auto">
+                    <a class="btn btn-danger" role="button" href="{{ route('col.client.table') }}">
+                        <p class="">Cancelar</p>
+                    </a>
                 </div>
             </div>
         </form>
+        @endforeach
       </div>
     </div>
-  </main> {{-- Fin del main --}}
+</div> {{-- Fin del div --}}
 @endsection {{-- Fin de la seccion --}}
