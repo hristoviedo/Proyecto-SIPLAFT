a0fae8240f71f8816522f9750c4ceadbcd827c99
Hristo Stochkov Oviedo Santos <hristo.oviedohn@gmail.com>
2020-03-03 09:33:50 -0600

    Update TransactionController.php
    Actualización de consultas para transacciones.

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -4,7 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Transaction;
 use Illuminate\Http\Request;
-
+use App\Client;
+use Illuminate\Support\Facades\Validator;
+use App\Funding;
+use App\Activity;
+use Carbon\Carbon;
+use App\Events\EventsSiplaft;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB; // Permite ejecutar consultas o llamar a procedimientos muy fácil
 
 //Inicio de la clase TransactionController
@@ -31,13 +37,13 @@ class TransactionController extends Controller
         ->join('users','transactions.user_id','=','users.id')
         ->join('clients','transactions.client_id','=','clients.id')
         ->join('companies','transactions.company_id','=','companies.id')
-        ->select('transactions.id AS transaction_id','transactions.transaction_apartment_number AS transaction_apartment_number',
-                'transactions.transaction_intermediary_bank AS transaction_intermediary_bank', 'transactions.transaction_operation_date AS transaction_operation_date',
-                'transactions.transaction_transfer_date AS transaction_transfer_date',
-                'transactions.transaction_cash AS transaction_cash', 'transactions.transaction_currency AS transaction_currency',
-                'transactions.transaction_amount AS transaction_amount', 'clients.identity AS client_identity', 'clients.name AS client_name',
+        ->select('transactions.id AS id','transactions.apartment_number AS apartment_number',
+                'transactions.intermediary_bank AS intermediary_bank', 'transactions.operation_date AS operation_date',
+                'transactions.transfer_date AS transfer_date',
+                'transactions.cash AS cash', 'transactions.currency AS currency',
+                'transactions.amount AS amount', 'clients.identity AS client_identity', 'clients.name AS client_name',
                 'users.name AS user_name', 'companies.name AS company_name')
-        ->orderByDesc('transaction_operation_date')
+        ->orderByDesc('operation_date')
         ->paginate(20);
 
         // Retorna la lista de transaccione, el total de registros y otros datos para la paginación
@@ -58,12 +64,12 @@ class TransactionController extends Controller
     public function indexTransactionsAll()
     {
         // Selecciona todas las transacciones de la tabla
-        $sql = 'SELECT tr.id AS transaction_id, tr.transaction_apartment_number AS transaction_apartment_number, tr.transaction_intermediary_bank AS transaction_intermediary_bank, tr.transaction_operation_date AS transaction_operation_date,
-                        tr.transaction_transfer_date AS transaction_transfer_date, tr.transaction_cash AS transaction_cash, tr.transaction_currency AS transaction_currency, tr.transaction_amount AS transaction_amount,
+        $sql = 'SELECT tr.id AS id, tr.apartment_number AS apartment_number, tr.intermediary_bank AS intermediary_bank, tr.operation_date AS operation_date,
+                        tr.transfer_date AS transfer_date, tr.cash AS cash, tr.currency AS currency, tr.amount AS amount,
                         cl.identity AS client_identity, cl.name AS client_name, co.name AS company_name, us.name AS user_name
                 FROM transactions tr, users us, companies co, clients cl
                 WHERE tr.user_id = us.id AND tr.company_id = co.id AND tr.client_id = cl.id
-                ORDER BY transaction_operation_date DESC';
+                ORDER BY operation_date DESC';
         $transactions = DB::select($sql);
 
         return $transactions; // Retorna la lista de transacciones
@@ -74,9 +80,66 @@ class TransactionController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request, $idClient)
     {
-        //
+        $user_modifier_id = Auth::user()->id;
+        $user_company = Auth::user()->company_id;
+        $clientData = DB::table('clients')
+                        ->where('id', $idClient)
+                        ->first();
+        $record_date = Carbon::now()->toDateTimeString();
+        $record_modified_register = null;
+        $record_new_data = null;
+        $record_old_data = null;
+
+        $validator = Validator::make($request->all(), [
+            'apartment_number'      => 'required|max:8',
+            'operation_date'        => 'required|date',
+            'transfer_date'         => 'date',
+            'intermediary_bank'     => 'string|max:20',
+            'amount'                => 'required|numeric',
+            'currency'              => 'string|max:20',
+            'cash'                  => 'boolean',
+        ]);
+
+        if ($validator->fails()) {
+            $record_action = 'FALLÓ REGISTRO DE TRANSACCIÓN';
+            $record_modified_table = null;
+            $data = array( 'user_modifier_id' => $user_modifier_id, 'record_action' => $record_action,
+            'record_date' => $record_date , 'record_modified_table' => $record_modified_table,
+            'record_modified_register' => $record_modified_register, 'record_new_data' => $record_new_data,
+            'record_old_data' => $record_old_data );
+            event( new EventsSiplaft( $data ));
+            $findLastRecord = DB::table('records')->latest('id')->first();
+            $deleteLastRecord = DB::table('records')->delete($findLastRecord->id);
+            return redirect('col.transaction.form')->withInput()->withErrors($validator);
+        }else{
+            Transaction::create([
+                'client_id'             => $idClient,
+                'user_id'               => $user_modifier_id,
+                'company_id'            => $user_company,
+                'activity_id'           => $clientData->activity_id,
+                'funding_id'            => $clientData->funding_id,
+                'apartment_number'      => $request['apartment_number'],
+                'operation_date'        => $request['operation_date'],
+                'transfer_date'         => $request['transfer_date'],
+                'intermediary_bank'     => trim(mb_strtoupper($request['intermediary_bank'])),
+                'amount'                => (float)$request['amount'],
+                'currency'              => trim(mb_strtoupper($request['currency'])),
+                'cash'                  => $request['cash'],
+                'workplace'             => $clientData->workplace,
+                'workstation'           => $clientData->workstation,
+                'salary'                => $clientData->salary,
+                ]);
+                $record_modified_table = 'TRANSACTIONS';
+                $record_action = 'REGISTRÓ NUEVA TRANSACCIÓN';
+                $data = array( 'user_modifier_id' => $user_modifier_id, 'record_action' => $record_action, 'record_date' => $record_date , 'record_modified_table' => $record_modified_table, 'record_modified_register' => $record_modified_register, 'record_new_data' => $record_new_data, 'record_old_data' => $record_old_data );
+                event( new EventsSiplaft( $data ));
+                $findLastRecord = DB::table('records')->latest('id')->first();
+                $deleteLastRecord = DB::table('records')->delete($findLastRecord->id);
+                $calculateData = DB::select('CALL calculateData (?)',array($idClient));
+                return back()->with('message', 'Transacción registrada'); //Retorna a la página anterior cuando registra al usuario
+        };
     }
 
     /**
