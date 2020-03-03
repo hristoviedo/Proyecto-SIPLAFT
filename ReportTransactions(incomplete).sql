892b59cba12fe9fdad22dbe1783d0690ddc01560
Hristo Stochkov Oviedo Santos <hristo.oviedohn@gmail.com>
2020-03-03 09:03:48 -0600

    SQL
    Actuaización de los procedimientos almacenado por cambios en la tabla transactions

--- ReportTransactions(incomplete).sql
+++ ReportTransactions(incomplete).sql
@@ -8,11 +8,11 @@ BEGIN
 	SELECT
     @rownum:= @rownum +1 AS NUMERO_REGISTRO,
 	cl.name AS CLIENTES, 
-	tr.transaction_apartment_number AS No_DE_APARTAMENTO,
-	tr.transaction_operation_date AS FECHAS_DE_OPERACION,
-	tr.transaction_amount AS MONTO_DE_OPERACION,
-	tr.transaction_transfer_date AS FECHA_DE_TRASPASO_DE_ESCRITURA,
-	IF(tr.transaction_intermediary_bank = "CONTADO","",tr.transaction_intermediary_bank) AS BANCO_INTERMEDIARIO,
+	tr.apartment_number AS No_DE_APARTAMENTO,
+	tr.operation_date AS FECHAS_DE_OPERACION,
+	tr.amount AS MONTO_DE_OPERACION,
+	tr.transfer_date AS FECHA_DE_TRASPASO_DE_ESCRITURA,
+	IF(tr.intermediary_bank = "CONTADO","",tr.intermediary_bank) AS BANCO_INTERMEDIARIO,
 	IF(fu.id = 5,"FINANCIAMIENTO","CONTADO") AS FONDOS,
 	IF(fu.id != 5, fu.name, "") AS FORMA_DE_PAGO,
 	tr.workplace AS LUGAR_DE_TRABAJO,
@@ -25,7 +25,7 @@ BEGIN
     ri.name AS RIESGO_CALCULADO
 	FROM clients cl, transactions tr,fundings fu,activities ac, risks ri
 	WHERE tr.company_id = companyID AND tr.client_id = cl.id AND tr.funding_id = fu.id AND tr.activity_id = ac.id AND cl.risk_id = ri.id
-	AND MONTH(tr.transaction_operation_date) = monthReport AND YEAR(tr.transaction_operation_date) = yearReport
+	AND MONTH(tr.operation_date) = monthReport AND YEAR(tr.operation_date) = yearReport
     ORDER BY NUMERO_REGISTRO;
 END$$
 DELIMITER ;
