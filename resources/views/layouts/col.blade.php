da1a07231f2004cd7b2917fd5543bb5a12c5234c
Hristo Stochkov Oviedo Santos <hristo.oviedohn@gmail.com>
2020-03-03 08:50:41 -0600

    Update col.blade.php
    Actualización de la plantilla para colaboradores.

--- resources/views/layouts/col.blade.php
+++ resources/views/layouts/col.blade.php
@@ -7,17 +7,31 @@
       <nav class="col-md-2 d-none d-md-block bg-light sidebar"> {{-- Inicio de la barra de navegación lateral --}}
         <div class="sidebar-sticky">
           <ul class="nav flex-column"> {{-- Inicio de la lista de elementos en la barra de navegación --}}
-            <li class="nav-item"> {{-- Item #1 página principal del colaborador --}}
-              <a class="nav-link link__color" href="col.client"> {{-- Enlazado a la ruta /home.col --}}
+            <li class="nav-item">
+				<a class="nav-link link__color" href="{{ route('col.client.table') }}">
+					<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+					Clientes <span class="sr-only">(current)</span>
+				</a>
+      </li>
+            {{--       
+            <li class="nav-item">
+              <a class="nav-link link__color" href="{{ route('col.client.form') }}">
                     <svg  width="20" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><line x1="17" y1="11" x2="7" y2="11"></line><line x1="12" y1="5" x2="12" y2="16"></line><circle cx="12" cy="11" r="11"></svg>
-                        Enviar Lista de Clientes <span class="sr-only">(current)</span>
+                        Crear Cliente <span class="sr-only">(current)</span>
               </a>
             </li>
-            <li class="nav-item"> {{-- Item #1 página principal del colaborador --}}
-              <a class="nav-link link__color" href="col.transaction"> {{-- Enlazado a la ruta /home.col --}}
+            <li class="nav-item">
+              <a class="nav-link link__color" href="col.transaction.form">
                     <svg  width="20" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><line x1="17" y1="11" x2="7" y2="11"></line><line x1="12" y1="5" x2="12" y2="16"></line><circle cx="12" cy="11" r="11"></svg>
-                        Enviar Lista de Transacciones <span class="sr-only">(current)</span>
+                        Crear Transacción <span class="sr-only">(current)</span>
               </a>
+            </li>
+             --}}
+            <li class="nav-item">
+                <a class="nav-link link__color" href="{{ route('col.transaction.table') }}">
+                    <svg  width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
+                Transacciones
+                </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link link__color" href="{{ route('col.simulation') }}">
